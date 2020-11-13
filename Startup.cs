d7f7b1a9dfd5febe75286c78164e22bc90d89244
mehtiumit <mehti.yldrm@gmail.com>
2020-11-13 14:58:31 +0300

    initial commit

--- Startup.cs
+++ Startup.cs
@@ -33,6 +33,7 @@ namespace ESShopReact.NetCore
         {
             if (env.IsDevelopment())
             {
+                ///
                 app.UseDeveloperExceptionPage();
             }
 
